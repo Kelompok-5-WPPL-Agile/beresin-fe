23f73a59d3e4be41e60508b0828f937bfdca2ad1
unknown <jefailhamprayoga13@gmail.com>
2024-04-05 20:54:54 +0700

    selesai slicing login register

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { EnvelopeIcon, LockClosedIcon } from "@heroicons/react/20/solid";
 
 export default function LoginPage() {
     const router = useRouter();
@@ -27,51 +28,100 @@ export default function LoginPage() {
     };
 
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content text-center">
-                <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">LOGIN</h1>
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input
-                                type="text"
-                                className="input input-bordered"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                                placeholder="Masukkan Alamat Email"
-                            />
+        <div>
+            <div className="flex flex-col px-48 py-12">
+                <div className="flex">
+                    <h1 className="font-extrabold text-xl">BERESIN</h1>
+                </div>
+                <div className="flex flex-row mt-24 justify-center validasi-content">
+                    <div className="flex w-1/2 flex-col pr-24 form-content">
+                        <h1 className="mx-auto font-extrabold text-4xl mb-12">WELCOME BACK</h1>
+                        <form onSubmit={handleSubmit}>
+                            <div className="form-control">
+                                <label className="form-control w-full max-w-full mb-2">
+                                    <div className="label mb-2">
+                                        <span className="label-text text-md font-medium">Email</span>
+                                    </div>
+                                    <div className="relative">
+                                        <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
+                                            <EnvelopeIcon className="w-5 h-5 opacity-70 text-primary" />
+                                        </span>
+                                        <input type="text" className="input input-bordered pl-10 w-full max-w-full" value={username} onChange={(e) => setUsername(e.target.value)} />
+                                    </div>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <label className="form-control w-full max-w-full mb-2">
+                                    <div className="label mb-2">
+                                        <span className="label-text text-md font-medium">Password</span>
+                                    </div>
+                                    <div className="relative">
+                                        <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
+                                            <LockClosedIcon className="w-5 h-5 opacity-70 text-primary" />
+                                        </span>
+                                        <input type="password" className="input input-bordered pl-10 w-full max-w-full" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                                    </div>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                            <button type="submit" className="btn btn-primary">LOGIN</button>
+                            </div>
+                        </form>
+                        <div className="mt-3">
+                            <h3 className="text-md font-medium">Belum punya akun? <a onClick={toRegister} className="text-blue-700 link no-underline">Daftar disini!</a></h3>
                         </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input
-                                type="password"
-                                className="input input-bordered"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                placeholder="Masukkan Password"
-                            />
-                        </div>
-                        <div className="form-control mt-6">
-                            <button type="submit" className="btn btn-primary">
-                                LOGIN
-                            </button>
-                        </div>
-                    </form>
-                    <div className="mt-3">
-                        <h3>
-                            Belum punya akun?{" "}
-                            <a onClick={toRegister} className="text-blue-700 link no-underline">
-                                Daftar disini!
-                            </a>
-                        </h3>
+                    </div>
+                    <div className="w-1/2 image-content">
+                        <img src="/assets/images/checklist.svg"></img>
                     </div>
                 </div>
             </div>
         </div>
+        // <div className="hero min-h-screen bg-base-200">
+        //     <div className="hero-content text-center">
+        //         <div className="max-w-md">
+        //             <h1 className="text-5xl font-bold">LOGIN</h1>
+        //             <form onSubmit={handleSubmit}>
+        //                 <div className="form-control">
+        //                     <label className="label">
+        //                         <span className="label-text">Email</span>
+        //                     </label>
+        //                     <input
+        //                         type="text"
+        //                         className="input input-bordered"
+        //                         value={username}
+        //                         onChange={(e) => setUsername(e.target.value)}
+        //                         placeholder="Masukkan Alamat Email"
+        //                     />
+        //                 </div>
+        //                 <div className="form-control">
+        //                     <label className="label">
+        //                         <span className="label-text">Password</span>
+        //                     </label>
+        //                     <input
+        //                         type="password"
+        //                         className="input input-bordered"
+        //                         value={password}
+        //                         onChange={(e) => setPassword(e.target.value)}
+        //                         placeholder="Masukkan Password"
+        //                     />
+        //                 </div>
+        //                 <div className="form-control mt-6">
+        //                     <button type="submit" className="btn btn-primary">
+        //                         LOGIN
+        //                     </button>
+        //                 </div>
+        //             </form>
+        //             <div className="mt-3">
+        //                 <h3>
+        //                     Belum punya akun?{" "}
+        //                     <a onClick={toRegister} className="text-blue-700 link no-underline">
+        //                         Daftar disini!
+        //                     </a>
+        //                 </h3>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     );
 }
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { EnvelopeIcon, LockClosedIcon , UserIcon , DevicePhoneMobileIcon} from "@heroicons/react/20/solid";
 
 export default function RegisterPage() {
     const router = useRouter();
@@ -29,75 +30,149 @@ export default function RegisterPage() {
     };
 
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content text-center">
-                <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">REGISTER</h1>
-                    <form onSubmit={handleSubmit}>
+        <div>
+            <div className="flex flex-col px-48 py-12">
+                <div className="flex">
+                    <h1 className="font-extrabold text-xl">BERESIN</h1>
+                </div>
+                <div className="flex flex-row mt-24 justify-center validasi-content">
+                    <div className="flex w-1/2 flex-col pr-24 form-content">
+                        <form onSubmit={handleSubmit}>
                         <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Name</span>
-                            </label>
-                            <input
-                                type="text"
-                                className="input input-bordered"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                placeholder="Masukkan Nama"
-                            />
+                                <label className="form-control w-full max-w-full mb-2">
+                                    <div className="label mb-2">
+                                        <span className="label-text text-md font-medium">Email</span>
+                                    </div>
+                                    <div className="relative">
+                                        <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
+                                            <EnvelopeIcon className="w-5 h-5 opacity-70 text-primary" />
+                                        </span>
+                                        <input type="text" className="input input-bordered pl-10 w-full max-w-full" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                    </div>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <label className="form-control w-full max-w-full mb-2">
+                                    <div className="label mb-2">
+                                        <span className="label-text text-md font-medium">Userame</span>
+                                    </div>
+                                    <div className="relative">
+                                        <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
+                                            <UserIcon className="w-5 h-5 opacity-70 text-primary" />
+                                        </span>
+                                        <input type="text" className="input input-bordered pl-10 w-full max-w-full" value={name} onChange={(e) => setName(e.target.value)} />
+                                    </div>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <label className="form-control w-full max-w-full mb-2">
+                                    <div className="label mb-2">
+                                        <span className="label-text text-md font-medium">Password</span>
+                                    </div>
+                                    <div className="relative">
+                                        <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
+                                            <LockClosedIcon className="w-5 h-5 opacity-70 text-primary" />
+                                        </span>
+                                        <input type="password" className="input input-bordered pl-10 w-full max-w-full" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                                    </div>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <label className="form-control w-full max-w-full mb-2">
+                                    <div className="label mb-2">
+                                        <span className="label-text text-md font-medium">Phone</span>
+                                    </div>
+                                    <div className="relative">
+                                        <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
+                                            <DevicePhoneMobileIcon className="w-5 h-5 opacity-70 text-primary" />
+                                        </span>
+                                        <input type="text" className="input input-bordered pl-10 w-full max-w-full" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                                    </div>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                            <button type="submit" className="btn btn-primary">Daftar</button>
+                            </div>
+                        </form>
+                        <div className="mt-3">
+                            <h3 className="text-md font-medium">Sudah punya akun? <a onClick={toLogin} className="text-blue-700 link no-underline">Masuk disini!</a></h3>
                         </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input
-                                type="text"
-                                className="input input-bordered"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                placeholder="Masukkan Alamat Email"
-                            />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input
-                                type="password"
-                                className="input input-bordered"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                placeholder="Masukkan Password"
-                            />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">phone</span>
-                            </label>
-                            <input
-                                type="text"
-                                className="input input-bordered"
-                                value={phone}
-                                onChange={(e) => setPhone(e.target.value)}
-                                placeholder="Masukkan Nomor HP"
-                            />
-                        </div>
-                        <div className="form-control mt-6">
-                            <button type="submit" className="btn btn-primary">
-                                Register
-                            </button>
-                        </div>
-                    </form>
-                    <div className="mt-3">
-                        <h3>
-                            Sudah punya akun?{" "}
-                            <a onClick={toLogin} className="text-blue-700 link no-underline">
-                                Masuk disini!
-                            </a>
-                        </h3>
+                    </div>
+                    <div className="w-1/2 image-content">
+                        <img src="/assets/images/checklist.svg"></img>
                     </div>
                 </div>
             </div>
         </div>
+        // <div className="hero min-h-screen bg-base-200">
+        //     <div className="hero-content text-center">
+        //         <div className="max-w-md">
+        //             <h1 className="text-5xl font-bold">REGISTER</h1>
+        //             <form onSubmit={handleSubmit}>
+        //                 <div className="form-control">
+        //                     <label className="label">
+        //                         <span className="label-text">Name</span>
+        //                     </label>
+        //                     <input
+        //                         type="text"
+        //                         className="input input-bordered"
+        //                         value={name}
+        //                         onChange={(e) => setName(e.target.value)}
+        //                         placeholder="Masukkan Nama"
+        //                     />
+        //                 </div>
+        //                 <div className="form-control">
+        //                     <label className="label">
+        //                         <span className="label-text">Email</span>
+        //                     </label>
+        //                     <input
+        //                         type="text"
+        //                         className="input input-bordered"
+        //                         value={email}
+        //                         onChange={(e) => setEmail(e.target.value)}
+        //                         placeholder="Masukkan Alamat Email"
+        //                     />
+        //                 </div>
+        //                 <div className="form-control">
+        //                     <label className="label">
+        //                         <span className="label-text">Password</span>
+        //                     </label>
+        //                     <input
+        //                         type="password"
+        //                         className="input input-bordered"
+        //                         value={password}
+        //                         onChange={(e) => setPassword(e.target.value)}
+        //                         placeholder="Masukkan Password"
+        //                     />
+        //                 </div>
+        //                 <div className="form-control">
+        //                     <label className="label">
+        //                         <span className="label-text">phone</span>
+        //                     </label>
+        //                     <input
+        //                         type="text"
+        //                         className="input input-bordered"
+        //                         value={phone}
+        //                         onChange={(e) => setPhone(e.target.value)}
+        //                         placeholder="Masukkan Nomor HP"
+        //                     />
+        //                 </div>
+        //                 <div className="form-control mt-6">
+        //                     <button type="submit" className="btn btn-primary">
+        //                         Register
+        //                     </button>
+        //                 </div>
+        //             </form>
+        //             <div className="mt-3">
+        //                 <h3>
+        //                     Sudah punya akun?{" "}
+        //                     <a onClick={toLogin} className="text-blue-700 link no-underline">
+        //                         Masuk disini!
+        //                     </a>
+        //                 </h3>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     );
 }
