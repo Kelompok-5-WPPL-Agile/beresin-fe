bb0273b34d60daa655a3306175761e3ab92b65d3
Dwiky Anggarda <dwiky.amin@gmail.com>
2024-04-06 22:11:19 +0700

    edit form task (done) sementara list task alakadarnya

--- src/app/task/page.js
+++ src/app/task/page.js
@@ -4,9 +4,9 @@ import AdminLayout from "../../components/AdminLayout";
 import { useState, useEffect, useCallback } from "react";
 import { apiFetch } from "@/libs/auth";
 import DataTable from "react-data-table-component";
-import { Squares2X2Icon } from "@heroicons/react/20/solid";
 import Swal from "sweetalert2";
 import moment from "moment";
+import Datepicker from "react-tailwindcss-datepicker";
 
 export default function TaskPage() {
     const router = useRouter();
@@ -19,13 +19,18 @@ export default function TaskPage() {
     const [description, setDescription] = useState('');
     const [category, setCategory] = useState(0);
     const [pic, setPic] = useState(0);
-    const [due_at, setDue_at] = useState('');
+    // const [due_at, setDue_at] = useState('');
+    const [due_at, setDue_at] = useState({ 
+        startDate: null,
+        endDate: null
+    });
     const [estimation, setEstimation] = useState('');
     const [priority, setPriority] = useState('');
     const [effort, setEffort] = useState('');
     const [status, setStatus] = useState('');
     const [formTitle, setFormTitle] = useState('');
     const [selectedRows, setSelectedRows] = useState([]);
+
 
     const fetchData = async () => {
         await apiFetch.get("/api/tasks").then((response) => {
@@ -38,13 +43,18 @@ export default function TaskPage() {
                     sortable: true,
                 },
                 {
+                    name: "PIC",
+                    selector: (row) => row.pic.name,
+                    sortable: true,
+                },
+                {
                     name: "Kategori",
-                    selector: (row) => row.category_id,
+                    selector: (row) => row.category.name,
                     sortable: true,
                 },
                 {
                     name: "Deadline",
-                    selector: (row) => row.due_at,
+                    selector: (row) => moment(row.due_at).format("DD/MM/YYYY"),
                     sortable: true,
                 },
                 {
@@ -95,7 +105,8 @@ export default function TaskPage() {
             pic_id: pic,
             title: title,
             description: description,
-            due_at: moment(due_at).format("YYYY-MM-DD HH:mm:ss"),
+            due_at: moment(due_at.startDate).format("YYYY-MM-DD HH:mm:ss"),
+            finished_at: moment(due_at.endDate).format("YYYY-MM-DD HH:mm:ss"),
             estimation: estimation,
             priority: priority,
             effort: effort,
@@ -139,30 +150,61 @@ export default function TaskPage() {
             })
     }
 
+    const handleDate = (newValue) => {
+        // console.log("newValue:", newValue);
+        setDue_at(newValue); 
+    }
+
     const handleRowSelected = useCallback((state) => {
         setSelectedRows(state.selectedRows);
     }, []);
 
     const handleEdit = () => {
-        setFormTitle("Form Ubah Task");
-        document.getElementById("title").value = selectedRows[0].title;
+        console.log('selected: ',selectedRows[0]);
+        setFormTitle('Form Ubah User')
+        document.getElementById('title').value = selectedRows[0].title;
+        document.getElementById('pic_id').value = selectedRows[0].pic_id;
+        document.getElementById('category_id').value = selectedRows[0].category_id;
+        // document.getElementById('due_at').value = moment(selectedRows[0].due_at).format("DD/MM/YYYY");
+        document.getElementById('description').value = selectedRows[0].description;
+        document.getElementById('estimation').value = selectedRows[0].estimation;
+        document.getElementById('priority').value = selectedRows[0].priority;
+        document.getElementById('effort').value = selectedRows[0].effort;
+        document.getElementById('status').value = selectedRows[0].status;
 
-        setTitle(selectedRows[0].title);
-
-        document.getElementById("my_modal_3").showModal();
+        setTitle(selectedRows[0].title)
+        setPic(selectedRows[0].pic_id)
+        setCategory(selectedRows[0].category_id)
+        // setDue_at(moment(selectedRows[0].due_at).format("DD/MM/YYYY"))
+        setDue_at({ 
+            startDate: moment(selectedRows[0].due_at).format("YYYY-MM-DD"),
+            endDate: moment(selectedRows[0].due_at).format("YYYY-MM-DD")
+        })
+        setDescription(selectedRows[0].description)
+        setEstimation(selectedRows[0].estimation)
+        setPriority(selectedRows[0].priority)
+        setEffort(selectedRows[0].effort)
+        setStatus(selectedRows[0].status)
+        
+        document.getElementById('my_modal_3').showModal()
     };
 
     const openModal = () => {
         setFormTitle("Form Tambah Task");
         document.getElementById("my_modal_3").showModal();
         document.getElementById("title").value = "";
-        document.getElementById("pic_id").value = "";
-        document.getElementById("category_id").value = "";
-        document.getElementById("due_at").value = "";
+        document.getElementById("pic_id").value = 0;
+        document.getElementById("category_id").value = 0;
+        setDue_at({ 
+            startDate: null,
+            endDate: null 
+        })
+        // document.getElementById("due_at").value = moment().format("DD/MM/YYYY");
+        document.getElementById("description").value = "";
         document.getElementById("estimation").value = "";
-        document.getElementById("priority").value = "";
-        document.getElementById("effort").value = "";
-        document.getElementById("status").value = "";
+        document.getElementById("priority").value = "null";
+        document.getElementById("effort").value = "null";
+        document.getElementById("status").value = "null";
     };
 
     const handleDelete = async () => {
@@ -219,13 +261,13 @@ export default function TaskPage() {
             <button className="btn btn-primary mb-4 mr-2" onClick={openModal}>
                 Tambah
             </button>
-            {/* <button
+            <button
                 type="button"
                 className="btn btn-neutral mr-2"
                 onClick={handleEdit}
             >
                 Edit
-            </button> */}
+            </button>
             <button type="button" className="btn btn-error" onClick={handleDelete}>
                 Hapus
             </button>
@@ -233,12 +275,14 @@ export default function TaskPage() {
                 <div className="modal-box">
                     <h3 className="font-bold text-lg mb-5">{formTitle}</h3>
                     <form method="dialog" onSubmit={submitForm}>
-                        <label className="input input-bordered flex items-center gap-2 mb-3">
-                            <Squares2X2Icon className="w-4 h-4 opacity-70" />
+                        <label className="form-control gap-2 mb-3">
+                            <div className="label">
+                                <span className="label-text">Judul</span>
+                            </div>
                             <input
                                 type="text"
                                 id="title"
-                                className="grow"
+                                className="input input-bordered"
                                 placeholder="Judul"
                                 autoComplete="off"
                                 onChange={(e) => setTitle(e.target.value)}
@@ -259,30 +303,43 @@ export default function TaskPage() {
                                 <span className="label-text">Kategori</span>
                             </div>
                             <select id="category_id" className="select select-bordered" onChange={(e) => setCategory(e.target.value)} required>
-                                <option disabled selected>Pilih Kategori</option>
+                                <option value={0} disabled selected>Pilih Kategori</option>
                                 {categories.map((category) => (
                                     <option value={category.id}>{category.name}</option>
                                 ))}
                             </select>
                         </label>
-                        <label className="input input-bordered flex items-center gap-2 mb-3">
-                            <Squares2X2Icon className="w-4 h-4 opacity-70" />
-                            <input
-                                type="date"
+                        <label className="form-control gap-2 mb-3">
+                            <div className="label">
+                                <span className="label-text">Deadline</span>
+                            </div>
+                            <Datepicker 
+                                useRange={false} 
+                                asSingle={true} 
+                                value={due_at} 
+                                onChange={handleDate}
+                                displayFormat={"DD/MM/YYYY"}
+                                inputClassName="input input-bordered w-full"
+                            />
+                            {/* <input type="text" id="due_at" placeholder="Deadline" onChange={(e) => setDue_at(e.target.value)} required></input> */}
+                            {/* <input
+                                type="text"
                                 id="due_at"
-                                className="grow"
+                                className="input input-bordered"
                                 placeholder="Deadline"
                                 autoComplete="off"
                                 onChange={(e) => setDue_at(e.target.value)}
                                 required
-                            />
+                            /> */}
                         </label>
-                        <label className="input input-bordered flex items-center gap-2 mb-3">
-                            <Squares2X2Icon className="w-4 h-4 opacity-70" />
+                        <label className="form-control gap-2 mb-3">
+                            <div className="label">
+                                <span className="label-text">Estimasi</span>
+                            </div>
                             <input
                                 type="text"
                                 id="estimation"
-                                className="grow"
+                                className="input input-bordered"
                                 placeholder="Estimasi"
                                 autoComplete="off"
                                 onChange={(e) => setEstimation(e.target.value)}
@@ -294,7 +351,7 @@ export default function TaskPage() {
                                 <span className="label-text">PIC</span>
                             </div>
                             <select id="pic_id" className="select select-bordered" onChange={(e) => setPic(e.target.value)} required>
-                                <option disabled selected>Pilih PIC</option>
+                                <option value={0} disabled selected>Pilih PIC</option>
                                 {users.map((user) => (
                                     <option value={user.id}>{user.name}</option>
                                 ))}
@@ -305,7 +362,7 @@ export default function TaskPage() {
                                 <span className="label-text">Prioritas</span>
                             </div>
                             <select id="priority" className="select select-bordered" onChange={(e) => setPriority(e.target.value)}>
-                                <option disabled selected>Pilih Prioritas</option>
+                                <option value={'null'} disabled selected>Pilih Prioritas</option>
                                 <option value={'normal'}>Normal</option>
                                 <option value={'high'}>High</option>
                                 <option value={'urgent'}>Urgent</option>
@@ -316,7 +373,7 @@ export default function TaskPage() {
                                 <span className="label-text">Effort</span>
                             </div>
                             <select id="effort" className="select select-bordered" onChange={(e) => setEffort(e.target.value)}>
-                                <option disabled selected>Pilih Effort</option>
+                                <option value={'null'} disabled selected>Pilih Effort</option>
                                 <option value={'easy'}>Easy</option>
                                 <option value={'medium'}>Medium</option>
                                 <option value={'hard'}>Hard</option>
@@ -327,7 +384,7 @@ export default function TaskPage() {
                                 <span className="label-text">Status</span>
                             </div>
                             <select id="status" className="select select-bordered" onChange={(e) => setStatus(e.target.value)}>
-                                <option disabled selected>Pilih Status</option>
+                                <option value={'null'} disabled selected>Pilih Status</option>
                                 <option value={'open'}>Open</option>
                                 <option value={'resolved'}>Resolved</option>
                                 <option value={'in_progress'}>In Progress</option>
