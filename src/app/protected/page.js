21ec34d3a6ed2b6bb6b264c0e76ba9281e106379
unknown <jefailhamprayoga13@gmail.com>
2024-04-23 10:36:23 +0700

    membuat card total users,categories,task pada dashboard

--- src/app/protected/page.js
+++ src/app/protected/page.js
@@ -4,21 +4,25 @@ import { apiFetch, logout } from "@/libs/auth";
 import moment from "moment";
 import { useRouter } from "next/navigation";
 import AdminLayout from "../../components/AdminLayout"
+import { UserGroupIcon, Squares2X2Icon , PencilSquareIcon, DevicePhoneMobileIcon} from "@heroicons/react/20/solid";
 
 export default function ProtectedPage() {
     const router = useRouter();
+
     // moment.locale("id");
     const [categories, setCategories] = useState([]);
-    const fetchData = async () => {
-        // console.log(moment().seconds(+30).format());
-        //fetch user from Rest API
-        await apiFetch.get('/api/categories')
-            .then((response) => {
-                //set response user to state
-                setCategories(response.data);
-                console.log('categories ', categories);
-            })
-    }
+    const [users, setUsers] = useState([]);
+    const [tasks, setTasks] = useState([]);
+    // const fetchData = async () => {
+    //     // console.log(moment().seconds(+30).format());
+    //     //fetch user from Rest API
+    //     await apiFetch.get('/api/categories')
+    //         .then((response) => {
+    //             //set response user to state
+    //             setCategories(response.data);
+    //             console.log('categories ', categories);
+    //         })
+    // }
 
     const logoutHandler = async () => {
         await logout();
@@ -39,17 +43,76 @@ export default function ProtectedPage() {
         //     })
     }
 
+    const fetchUsers = async () => {
+      await apiFetch.get("/api/users").then((response) => {
+          //console.log(response);
+          setUsers(response.data.data);
+      });
+  };
+
+  const fetchCategories = async () => {
+    await apiFetch.get("/api/categories").then((response) => {
+        //console.log(response);
+        setCategories(response.data.data);
+    });
+};
+
+const fetchTask = async () => {
+  await apiFetch.get("/api/tasks").then((response) => {
+      //console.log(response);
+      setTasks(response.data.data);
+  });
+};
+
+
     //hook useEffect
     useEffect(() => {
         // console.log(moment().seconds(30).format());
         //call function "fetchData"
-        fetchData();
+        //fetchData();
+        fetchUsers();
+        fetchCategories();
+        fetchTask();
     }, []);
 
     return (
       <>
         <AdminLayout router={router}>
-          <h1>Dashboard page</h1>
+          <div className="flex flex-row justify-around card-content">
+          <div className="card card-total card-side bg-base-100 shadow-xl w-1/3 py-4 pl-2 ">
+            <div className="flex flex-row">
+              <div className="flex flex-col justify-center">
+                  <UserGroupIcon className="w-16 h-16 text-primary" />
+              </div>
+              <div className="flex flex-col pl-2 justify-center">
+                  <h1 className="font-semibold text-xl">{users.length}</h1>
+                  <p>Total Users</p>
+              </div>
+            </div>
+          </div>
+          <div className="card card-total card-side bg-base-100 shadow-xl w-1/3 py-2 pl-2  ml-2">
+            <div className="flex flex-row">
+              <div className="flex flex-col justify-center">
+                  <Squares2X2Icon className="w-16 h-16 text-primary" />
+              </div>
+              <div className="flex flex-col pl-2 justify-center">
+                  <h1 className="font-semibold text-xl">{categories.length}</h1>
+                  <p>Total Categories</p>
+              </div>
+            </div>
+          </div>
+          <div className="card card-total card-side bg-base-100 shadow-xl w-1/3 py-2 pl-2  ml-2">
+            <div className="flex flex-row">
+              <div className="flex flex-col justify-center">
+                  <PencilSquareIcon className="w-16 h-16 text-primary" />
+              </div>
+              <div className="flex flex-col pl-2 justify-center">
+                  <h1 className="font-semibold text-xl">{tasks.length}</h1>
+                  <p>Total Tasks</p>
+              </div>
+            </div>
+          </div>
+          </div>
           {/* <p>Categories</p> */}
           <ul>
             {/* {categories.map((category) => (
