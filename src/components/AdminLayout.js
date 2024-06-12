0cb434433412d421f55f79c2eee5351a09bf6b80
unknown <jefailhamprayoga13@gmail.com>
2024-06-12 14:51:37 +0700

    fix header

--- src/components/AdminLayout.js
+++ src/components/AdminLayout.js
@@ -3,40 +3,43 @@ import { usePathname, useRouter } from "next/navigation";
 import { MENU_LIST } from "./constants";
 import styles from "../app/globals.css";
 import { BellIcon, Bars3BottomLeftIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
-import {  logout, apiFetch} from "@/libs/auth";
+import { logout, apiFetch } from "@/libs/auth";
 
-export default function Layout({ children}) {
+export default function Layout({ children }) {
   const router = useRouter();
   const pathName = usePathname();
   const [activePath, setActivePath] = useState(pathName);
   const [activeMenu, setActiveMenu] = useState("");
   const [isOpen, setIsOpen] = useState(false);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
   const [avatarPlaceholder, setAvatarPlaceholder] = useState("");
-
+  const [isLeader, setIsLeader] = useState(null);
 
   const fetchData = async () => {
     await apiFetch.get("/api/profile").then((response) => {
-        //console.log(response);
-        setData(response.data.data);
-
-        let word = response.data.data.name;
-        var firstLetter = word.charAt(0);
-
-        // Mengonversi huruf pertama menjadi huruf kapital
-        setAvatarPlaceholder(firstLetter.toUpperCase());
+      console.log(response);
+      const profileData = response.data.data;
+      setData(profileData);
+      setIsLeader(profileData.is_leader);
+      localStorage.setItem("isLeader", profileData.is_leader); // Save is_leader to local storage
+      let word = profileData.name;
+      var firstLetter = word.charAt(0);
+      setAvatarPlaceholder(firstLetter.toUpperCase());
     });
-};
+  };
 
-
-useEffect(() => {
+  useEffect(() => {
+    const cachedIsLeader = localStorage.getItem("isLeader");
+    if (cachedIsLeader) {
+      setIsLeader(parseInt(cachedIsLeader)); // Retrieve is_leader from local storage
+    }
     fetchData();
-}, []);
+  }, []);
 
   useEffect(() => {
     const handleOutsideClick = (event) => {
       const aside = document.getElementById("beresin-sidebar");
-      if (!aside.contains(event.target)) {
+      if (aside && !aside.contains(event.target)) {
         setIsOpen(false);
       }
     };
@@ -61,7 +64,7 @@ useEffect(() => {
 
   const handleChangePage = (path) => {
     router.push(path);
-    setIsOpen(false); 
+    setIsOpen(false);
   };
 
   const toProfile = () => {
@@ -70,10 +73,9 @@ useEffect(() => {
 
   const logoutHandler = async () => {
     await logout();
+    localStorage.removeItem("isLeader");
     router.push("/login");
-}
-
-
+  };
 
   return (
     <div className="w-full h-full relative flex-1 flex-row">
@@ -84,7 +86,7 @@ useEffect(() => {
         aria-controls="beresin-sidebar"
         type="button"
         className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        onClick={() => setIsOpen(!isOpen)} 
+        onClick={() => setIsOpen(!isOpen)}
       >
         <span className="sr-only">Open sidebar</span>
         <Bars3BottomLeftIcon className=" w-8 h-8" />
@@ -103,7 +105,25 @@ useEffect(() => {
         <div className="h-full px-6 py-4 overflow-y-auto">
           <ul>
             {MENU_LIST.map((menu, index) => {
-              if (menu.name !== "Profile") {
+              if (isLeader !== 1) {
+                if (menu.name !== "Profile" && menu.name !== "Daftar Kategori" && menu.name !== "Daftar User") {
+                  return (
+                    <li
+                      key={index}
+                      className={
+                        activePath === menu.path
+                          ? "flex flex-row items-center px-3 py-1 rounded-md active mb-4"
+                          : "flex flex-row items-center px-3 py-1 rounded-md mb-4"
+                      }
+                      onClick={() => handleChangePage(menu.path)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      {menu.icon && <menu.icon className="w-5 mr-2" />}
+                      {menu.name}
+                    </li>
+                  );
+                }
+              } else if (menu.name !== "Profile") {
                 return (
                   <li
                     key={index}
@@ -112,7 +132,7 @@ useEffect(() => {
                         ? "flex flex-row items-center px-3 py-1 rounded-md active mb-4"
                         : "flex flex-row items-center px-3 py-1 rounded-md mb-4"
                     }
-                    onClick={() => handleChangePage(menu.path, menu.name)}
+                    onClick={() => handleChangePage(menu.path)}
                     style={{ cursor: "pointer" }}
                   >
                     {menu.icon && <menu.icon className="w-5 mr-2" />}
@@ -132,21 +152,24 @@ useEffect(() => {
             {activeMenu ? activeMenu : ""}
           </h1>
           <div className="flex flex-row items-center">
-            <BellIcon className=" w-8 h-8 mr-3" />
             <div className="avatar placeholder">
-                <div className="bg-neutral text-neutral-content rounded-full w-10 rounded-full">
-                    <span className="text-xl">{avatarPlaceholder}</span>
-                {/* <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" /> */}
-                </div>
+              <div className="bg-neutral text-neutral-content rounded-full w-10 rounded-full">
+                <span className="text-xl">{avatarPlaceholder}</span>
+              </div>
             </div>
             <details className="dropdown ">
-              <summary className="btn ml-3 btn-primary">{data.name}<ChevronDownIcon className=" w-4 h-4" /></summary>
+              <summary className="btn ml-3 btn-primary">
+                {data.name}
+                <ChevronDownIcon className=" w-4 h-4" />
+              </summary>
               <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
                 <li>
                   <a onClick={toProfile}>Profile</a>
                 </li>
                 <li>
-                  <a className="text-red-500" onClick={logoutHandler}>Logout</a>
+                  <a className="text-red-500" onClick={logoutHandler}>
+                    Logout
+                  </a>
                 </li>
               </ul>
             </details>
@@ -156,4 +179,4 @@ useEffect(() => {
       </div>
     </div>
   );
-};
+}
